9337c281ce8efc6cc2ab09ff31b2d9b4684ec8d8
Michael Doroff <michael_doroff@yahoo.com>
2017-10-28 16:06:20 -0500

    Added Stackview for email/password login details

---
+++ Cudate!/CustomButtons.swift
@@ -0,0 +1,28 @@
+//
+//  CustomButtons.swift
+//  Cudate!
+//
+//  Created by Michael Doroff on 10/26/17.
+//  Copyright © 2017 Michael Doroff. All rights reserved.
+//
+
+import UIKit
+import LBTAComponents
+import FBSDKLoginKit
+import GoogleSignIn
+
+
+let FBLoginButton: FBSDKLoginButton = {
+    let FBbutton = FBSDKLoginButton()
+    FBbutton.translatesAutoresizingMaskIntoConstraints = false
+    FBbutton.readPermissions = ["email", "public_profile"]
+    return FBbutton
+    
+}()
+
+let GoogleSignIn: GIDSignInButton = {
+    let Googlebutton = GIDSignInButton()
+    Googlebutton.translatesAutoresizingMaskIntoConstraints = false
+    return Googlebutton
+    
+}()
--- Cudate!/ViewController.swift
+++ Cudate!/ViewController.swift
@@ -11,31 +11,120 @@ import LBTAComponents
 import FBSDKLoginKit
 
 
-let FBLoginButton: FBSDKLoginButton = {
-    let button = FBSDKLoginButton()
-    button.readPermissions = ["email", "public_profile"]
-    return button
-    
-}()
-
-
 
 class LoginViewController: UIViewController {
+    
+    
+    let inputsContainerView: UIView = {
+       let view = UIView()
+       view.backgroundColor = .white
+       view.translatesAutoresizingMaskIntoConstraints = false
+       view.layer.cornerRadius = 5
+       view.layer.masksToBounds = true
+       return view
+        
+    }()
 
+    let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        button.setTitle("Register", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+        
+    }()
+    
+    let nameTextField: UITextField = {
+       let tf = UITextField()
+       tf.placeholder = "Name"
+       tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.translatesAutoresizingMaskIntoConstraints  = false
+        return tf
+        
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        view.addSubview(FBLoginButton)
-        
-        
-        
+//        view.addSubview(FBLoginButton)
+//        view.addSubview(GoogleSignIn)
         view?.backgroundColor = UIColor(r: 232, g: 236, b: 241)
+//        view.addSubview(inputsContainerView)
+//        view.addSubview(registerButton)
+//        view.addSubview(nameTextField)
+        
+        setUpEmailPasswordFields()
+        
+  
+//        
+//        inputsContainerView.anchor(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: view.frame.height / 2, leftConstant: 12, bottomConstant: view.frame.height / 4, rightConstant: 12, widthConstant: 0, heightConstant: 0)
+        
+//        nameTextField.anchor(inputsContainerView.topAnchor, left: inputsContainerView.leftAnchor, bottom: nil, right: inputsContainerView.rightAnchor, topConstant: 5, leftConstant: 5, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+//        registerButton.anchor(inputsContainerView.bottomAnchor, left: inputsContainerView.leftAnchor, bottom: nil, right: inputsContainerView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
         
         
-        FBLoginButton.anchor(nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 0, leftConstant: 20, bottomConstant: 60, rightConstant: 20, widthConstant: 20, heightConstant: 70)
+ 
         
         
+        
+//        
+//        FBLoginButton.anchor(nil, left: view.leftAnchor, bottom: GoogleSignIn.topAnchor, right: view.rightAnchor, topConstant: 0, leftConstant: 20, bottomConstant: 40, rightConstant: 20, widthConstant: 0, heightConstant: 70)
+//        
+//        GoogleSignIn.anchor(FBLoginButton.bottomAnchor, left: FBLoginButton.leftAnchor, bottom: view.bottomAnchor, right: FBLoginButton.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+//        
     
+    }
+    
+    public func setUpEmailPasswordFields() {
+        
+        let nameContainerView = UIView()
+        nameContainerView.backgroundColor = .red
+        nameContainerView.translatesAutoresizingMaskIntoConstraints = false
+        nameContainerView.layer.cornerRadius = 5
+        nameContainerView.layer.masksToBounds = true
+        
+        let emailContainerView = UIView()
+        emailContainerView.backgroundColor = .blue
+        emailContainerView.translatesAutoresizingMaskIntoConstraints = false
+        emailContainerView.layer.cornerRadius = 5
+        emailContainerView.layer.masksToBounds = true
+        
+        
+        let passwordContainerView = UIView()
+        passwordContainerView.backgroundColor = .black
+        passwordContainerView.translatesAutoresizingMaskIntoConstraints = false
+        passwordContainerView.layer.cornerRadius = 5
+        passwordContainerView.layer.masksToBounds = true
+        
+        let loginStackView = UIStackView(arrangedSubviews: [nameContainerView, emailContainerView, passwordContainerView])
+        loginStackView.translatesAutoresizingMaskIntoConstraints = false
+        loginStackView.layer.cornerRadius = 5
+
+        loginStackView.layer.masksToBounds = true
+        
+        loginStackView.axis = .vertical
+        loginStackView.distribution = .fillEqually
+        
+        view.addSubview(loginStackView)
+        
+        loginStackView.anchor(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: view.frame.height / 2, leftConstant: 12, bottomConstant: view.frame.height / 4, rightConstant: 12, widthConstant: 0, heightConstant: 0)
+        
+
+        
+        
+
+        
     }
 
     
