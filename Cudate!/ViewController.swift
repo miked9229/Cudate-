6625ba139804a2714ab8a676ec60d400eb390f46
Michael Doroff <michael_doroff@yahoo.com>
2017-10-29 20:57:42 -0500

    Set up the segment control

--- Cudate!/ViewController.swift
+++ Cudate!/ViewController.swift
@@ -94,7 +94,22 @@ class LoginViewController: UIViewController {
         return button
     }()
     
+    lazy var loginRegisterSegmentedControl: UISegmentedControl = {
+        let sc = UISegmentedControl(items: ["Login", "Register'"])
+        sc.translatesAutoresizingMaskIntoConstraints = false
+        sc.tintColor = .white
+        sc.selectedSegmentIndex = 1
+        sc.addTarget(self, action: #selector(handleLoginRegisterChange), for: .valueChanged)
+        return sc
+        
+        
+    }()
     
+    func handleLoginRegisterChange() {
+        
+        
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -106,44 +121,24 @@ class LoginViewController: UIViewController {
 //        view.addSubview(nameTextField)
         
         setUpEmailPasswordFields()
-        
-  
-//        
-//        inputsContainerView.anchor(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: view.frame.height / 2, leftConstant: 12, bottomConstant: view.frame.height / 4, rightConstant: 12, widthConstant: 0, heightConstant: 0)
-        
-//        nameTextField.anchor(inputsContainerView.topAnchor, left: inputsContainerView.leftAnchor, bottom: nil, right: inputsContainerView.rightAnchor, topConstant: 5, leftConstant: 5, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
-        
-//        registerButton.anchor(inputsContainerView.bottomAnchor, left: inputsContainerView.leftAnchor, bottom: nil, right: inputsContainerView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+
         
         
- 
-        
-        
-        
-//        
-//        FBLoginButton.anchor(nil, left: view.leftAnchor, bottom: GoogleSignIn.topAnchor, right: view.rightAnchor, topConstant: 0, leftConstant: 20, bottomConstant: 40, rightConstant: 20, widthConstant: 0, heightConstant: 70)
-//        
-//        GoogleSignIn.anchor(FBLoginButton.bottomAnchor, left: FBLoginButton.leftAnchor, bottom: view.bottomAnchor, right: FBLoginButton.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
-//        
-    
     }
     
     public func setUpEmailPasswordFields() {
         
         let nameContainerView = UIView()
-//        nameContainerView.backgroundColor = .red
         nameContainerView.translatesAutoresizingMaskIntoConstraints = false
         nameContainerView.layer.cornerRadius = 5
         nameContainerView.layer.masksToBounds = true
         
         let emailContainerView = UIView()
-//        emailContainerView.backgroundColor = .blue
         emailContainerView.translatesAutoresizingMaskIntoConstraints = false
         emailContainerView.layer.cornerRadius = 5
         emailContainerView.layer.masksToBounds = true
         
         let passwordContainerView = UIView()
-//        passwordContainerView.backgroundColor = .black
         passwordContainerView.translatesAutoresizingMaskIntoConstraints = false
         passwordContainerView.layer.cornerRadius = 5
         passwordContainerView.layer.masksToBounds = true
